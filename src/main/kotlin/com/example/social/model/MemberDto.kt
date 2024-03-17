91cd49022ef7548184f70186ff972a8b0a43564a
J-HyeonSeo <jcs2349@gmail.com>
2024-03-18 08:22:19 +0900

    Feat: 회원 관련 Dto data class 추가

--- src/main/kotlin/com/example/social/model/MemberDto.kt
+++ src/main/kotlin/com/example/social/model/MemberDto.kt
@@ -1,4 +1,21 @@
 package com.example.social.model
 
-class MemberDto {
-}
+data class MemberResisterDto(
+    val email: String,
+    val password: String,
+    val name: String,
+    val profileUrl: String
+)
+
+data class MemberUpdateDto(
+    val email: String,
+    val name: String,
+    val profileUrl: String
+)
+
+data class MemberDto(
+    val id: Int,
+    val email: String,
+    val name: String,
+    val profileUrl: String
+)
