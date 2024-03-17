5a69712ac56ed56a6cc5985a17cac31933824cde
J-HyeonSeo <jcs2349@gmail.com>
2024-03-18 08:22:02 +0900

    Feat: MyBatis 회원관련 SQL Mapper 추가

--- src/main/kotlin/com/example/social/mapper/SocialMapper.kt
+++ src/main/kotlin/com/example/social/mapper/SocialMapper.kt
@@ -1,4 +1,18 @@
 package com.example.social.mapper
 
-class SocialMapper {
+import com.example.social.model.MemberDto
+import com.example.social.model.MemberResisterDto
+import com.example.social.model.MemberUpdateDto
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
+
+@Mapper
+interface SocialMapper {
+
+    fun existsByEmail(@Param("email") email: String)
+    fun selectMemberByEmail(@Param("email") email: String): MemberDto
+    fun insertMember(registerDto: MemberResisterDto)
+    fun updateMember(updateDto: MemberUpdateDto)
+
+
 }
