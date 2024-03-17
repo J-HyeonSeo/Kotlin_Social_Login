a72e20887c89df382fc4cb954eeb538b20ead40a
J-HyeonSeo <jcs2349@gmail.com>
2024-03-18 08:23:14 +0900

    Feat: 프로젝트 Config 및 메인 클래스 추가.

--- src/main/kotlin/com/example/social/config/AppConfig.kt
+++ src/main/kotlin/com/example/social/config/AppConfig.kt
@@ -1,4 +1,16 @@
 package com.example.social.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
 class AppConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder();
+    }
+
 }
--- src/main/kotlin/com/example/social/controller/SocialController.kt
+++ src/main/kotlin/com/example/social/controller/SocialController.kt
@@ -1,4 +1,10 @@
 package com.example.social.controller
 
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class SocialController {
+
+
+
 }
