1b377a8590e294e0bb2cc2a5d1ce5ac03d6f9fe5
J-HyeonSeo <jcs2349@gmail.com>
2024-03-19 20:32:38 +0900

    Feat: 리액트 StrictMode 해제 및, 토큰 LocalStorage에 추가.

--- react-app/src/pages/LoginProcessPage.js
+++ react-app/src/pages/LoginProcessPage.js
@@ -8,11 +8,12 @@ const LoginProcessPage = () => {
   fetch("http://localhost:8080" + window.location.pathname + window.location.search, {
     method: "POST",
     headers: { 'Content-Type': 'application/json' },
-    credentials: 'include'
+    credentials: 'include' 
   })
   .then(res => {
     res.json().then(json => {
       console.log(json);
+      window.localStorage.setItem("token", json.token);
       navigate("/");
     })
   }).catch(e => {
